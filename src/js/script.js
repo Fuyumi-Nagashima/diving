0588a3514e754062b4d289f0893c194e88ea03f3
Fuyu <m.fuyumi18@gmail.com>
2024-02-23 16:19:18 +0900

    2024-02-23

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,27 @@
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) { 
+    $(".js-hamburger").on("click", function () {
+        $(".js-hamburger").toggleClass("is-active");
+        $(".js-drawer").fadeToggle();
+    });
+
+$(".js-drawer a[href]").on("click", function () {
+    $(".js-hamburger").trigger("click");
+});
+  //ハンバーガーメニューを開いている時のスクロールを禁止する(背景固定)
+let state = false;
+let pos;
+$(".js-hamburger").on("click", function () {
+    if (state == false) {
+    pos = $(window).scrollTop();
+    $("body").addClass("js-fixed").css({ top: -pos });
+    state = true;
+    } else {
+    $("body").removeClass("js-fixed").css({ top: 0 });
+    window.scrollTo(0, pos);
+    state = false;
+    }
+});
+
     
 });
